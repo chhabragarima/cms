0c2142830aaae912dbd3c5f31f8df44b44a93d09
chhabragarima <chhabragarima15@gmail.com>
2018-03-22 21:10:37 -0600

    php project 1st commit

---
+++ admin/categories.php
@@ -0,0 +1,44 @@
+<?php include "includes/header.php";
+?>
+    <div id="wrapper">
+
+        <!-- Navigation -->
+<?php
+        include "includes/navigation.php";
+        ?>      
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+                           
+                           <div class="col-xs-6">
+                           <form action="">
+                               <div class="form-group">
+                                   <label for="cat-title">Add Category</label>
+                               <input type="text" class="form-control" name="cat_title">
+                               </div>
+                                    <div class="form-group">
+                               <input type="submit" name="submit" value="Add Category" class="btn btn-primary">
+                               </div>
+                           
+                           </form>
+                           
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+<?php include "includes/footer.php";
+?>
+  
